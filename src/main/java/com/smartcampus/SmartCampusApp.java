b5b73dbad787d4c1f580f5ca434b57251d14ef06
styloq99-arch <chanukisemini16@gmail.com>
2026-05-01 08:02:27 +0530

    Fix SmartCampusApp to explicitly register classes

--- src/main/java/com/smartcampus/SmartCampusApp.java
+++ src/main/java/com/smartcampus/SmartCampusApp.java
@@ -1,7 +1,9 @@
 package com.smartcampus;
 
 import javax.ws.rs.core.Application;
+import javax.ws.rs.ApplicationPath;
 
+@ApplicationPath("/api/v1")
 public class SmartCampusApp extends Application {
     // path is defined in web.xml to avoid double registration conflict
 }
